dc23699cd423292de0602720a7d8cc5d59092b09
ei17ringo <ei17ringo@gmail.com>
2014-11-12 08:37:55 +0800

    Initial Commit

---
+++ SampleFavoriteList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleFavoriteList
+//
+//  Created by Eriko Ichinohe on 2014/11/12.
+//  Copyright (c) 2014年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleFavoriteList/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleFavoriteList
+//
+//  Created by Eriko Ichinohe on 2014/11/12.
+//  Copyright (c) 2014年 Eriko Ichinohe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
